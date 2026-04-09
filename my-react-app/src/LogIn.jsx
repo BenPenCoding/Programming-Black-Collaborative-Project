7b2ed051dada1f9908b28f5d43a63a4f275e2507
Mark Watson <mark2736.watson@gmail.com>
2026-04-09 17:45:54 +0100

    Created frontend and added login page

---
+++ my-react-app/src/LogIn.jsx
@@ -0,0 +1,47 @@
+import { Link } from "react-router-dom";
+
+function LogIn() {
+  return (
+    <div className="container p-5 my-5 border">
+        <h1 className="my-3">Log In</h1>
+        <Username />
+        <Password />
+        <Submit />
+        <SignUpLink />
+    </div>
+  );
+}
+
+function Username() {
+  return (
+    <div className="form-group my-2">
+        <label htmlFor="username" className="form-label">Username:</label>
+        <input type="text" className="form-control" id="username" placeholder="Enter Username" />
+    </div>
+  );
+}
+
+function Password() {
+  return (
+    <div className="form-group my-2">
+     <label htmlFor="password" className="form-label">Password:</label>
+     <input type="password" className="form-control" id="password" placeholder="Enter Password" />
+    </div>
+  );
+}
+
+function Submit() {
+  return (
+    <button type="submit" className="btn btn-primary my-2">Submit</button>
+  );
+}
+
+function SignUpLink() {
+  return (
+    <>
+     <p className="fs-5 my-2">Don't have an account? <a href="/signup">Sign Up</a></p>
+    </>
+  );
+}
+
+export default LogIn;
---
+++ my-react-app/src/SignUp.jsx
@@ -0,0 +1,57 @@
+import { Link } from "react-router-dom";
+
+function SignUp() {
+  return (
+    <div className="container p-5 my-5 border">
+        <h1 className="my-3">Sign Up</h1>
+        <Username />
+        <Name />
+        <Password />
+        <Submit />
+        <LogInLink />
+    </div>
+  );
+}
+
+function Username() {
+  return (
+    <div className="form-group my-2">
+        <label htmlFor="username" className="form-label">Username:</label>
+        <input type="text" className="form-control" id="username" placeholder="Enter Username" />
+    </div>
+  );
+}
+
+function Name() {
+  return (
+    <div className="form-group my-2">
+        <label htmlFor="name" className="form-label">Name:</label>
+        <input type="text" className="form-control" id="name" placeholder="Enter Name" />
+    </div>
+  );
+}
+
+function Password() {
+  return (
+    <div className="form-group my-2">
+     <label htmlFor="password" className="form-label">Password:</label>
+     <input type="password" className="form-control" id="password" placeholder="Enter Password" />
+    </div>
+  );
+}
+
+function Submit() {
+  return (
+    <button type="submit" className="btn btn-primary my-2">Submit</button>
+  );
+}
+
+function LogInLink() {
+  return (
+    <>
+     <p className="fs-5 my-2">Already have an account? <a href="/">Log In</a></p>
+    </>
+  );
+}
+
+export default SignUp;
